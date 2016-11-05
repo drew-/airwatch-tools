4c09bcf64ba0ed3a7df314cd709030739c92d29e
Andy Myers <jamyers@andy.local>
2016-11-05 18:11:15 -0400

    Added toolstrip with Open CSV, Exit, Visualize and About buttons.  Added Add User textbox and button.  Added Edit The List Textbox and Create CSV and Append CSV buttons.  Added Cosole Output textbox. Added Wolf Logo.  Wired empty event handlers for buttons and toolstrip items.

--- AirwatchTools/MainWindow.xaml.cs
+++ AirwatchTools/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AirwatchTools {
     /// <summary>
@@ -20,6 +7,24 @@ namespace AirwatchTools {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        //TOOLBAR MENU EVENT HANDLERS
+        private void btnCreateCsv_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Visualize_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        //MAIN PAGE BUTTON EVENT HANDLERS
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
